e955d30f5d0b5f662fedc4695e4a4feb27adad5c
Sujal sharma <imsharmasujal@gmail.com>
2026-03-31 20:28:06 +0530

    Job openings card project ui

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
-      
+    <div className="card">
+      <div>
+        <div className="top">
+          <img src={props.img} alt="companyImg" />
+          <button>
+            Save
+            <Bookmark size={12} />
+          </button>
+        </div>
+        <div className="center">
+          <h3>
+            {props.company} <span>{props.datePstd}</span>
+          </h3>
+          <h2>{props.post}</h2>
+          <div className="tag">
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div>
+          <h3>{props.pay}</h3>
+          <p>{props.location}</p>
+        </div>
+        <button>Apply Now</button>
+      </div>
     </div>
   )
 }
